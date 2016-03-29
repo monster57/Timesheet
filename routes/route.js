bc1ea99eb34e07a24375fbab2c762cfaf3319e97
monster57 <surajitbarman57@yahoo.in>
2016-03-29 21:14:29 +0530

    added user controller to routes

--- routes/route.js
+++ routes/route.js
@@ -1,5 +1,6 @@
 var models  = require('../models');
 var express = require('express');
+var userController = require('../controller/user_controller').userController;
 var router = express.Router();
 
 /* GET home page. */
@@ -14,7 +15,7 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.get('/register', function(req,res,next){
-	res.render('register')
-})
+router.get('/register', userController.registerUser)
+
+
 module.exports = router;
